113b2876861ca1880b5c4f446e32e2aeb7c55a78
ANtibushitse <ANtibushitse@users.noreply.github.com>
2016-10-07 09:42:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// good luck! ( merci )
 var all_Inputs = $("input");
 for (var i = 0; i < all_Inputs.length; i++) {
 	var couleurs = $(all_Inputs[i]).data("color")
-		// etape 2:  Un click sur le bouton doit mettre cette couleur en fond
 	$(all_Inputs[i]).css("background-color", couleurs);
 }
-// Je nomme la function Checkoed
+
 function checked() {
 	return $("#modify-texte").is(":checked")
 };
-//  
 $("input").click(function () {
 	var i_ClickOnTheColour = $(this).data("color");
 	if (checked()) {
@@ -19,5 +16,3 @@ $("input").click(function () {
 		$("body").css('background-color', i_ClickOnTheColour)
 	};
 });
-// ±if plan A don't  work ther is  differents alphabets  in the word±  
-//
